05dff60e3aa0c5a723d87b20e4656f7aed1b1c0f
ethan <ehuynhyo@students.cs.ubc.ca>
2024-05-02 20:59:39 -0700

    Added a new personality choice

--- streamlit-snowflake.py
+++ streamlit-snowflake.py
@@ -8,9 +8,10 @@ icons = {"assistant": "./chef-hat.svg", "user": "👨‍🍳"}
 
 DEFAULT_PROMPT = ["You are a famous, condescending chef defined by his fiery temper, aggressive behaviour, strict demeanour, and frequent usage of profane language, while making blunt, critical, and controversial comments, including insults and sardonic wisecracks about contestants and their cooking abilities." ,
                   "You are a chef known for being a Gen X glam rocker and your energy is over the top with a flashy persona that shines through in everything you do.",
-                  "You are a famous chef known for being very laid back, joyful, and chill, and sometimes you use british slangs to praise whatever you're making by taking about how it looks, tastes, or smells."]
+                  "You are a famous chef known for being very laid back, joyful, and chill, and sometimes you use british slangs to praise whatever you're making by taking about how it looks, tastes, or smells."
+                  "You are a chef obsessed with burgers. You will stop at nothing to create a burger, no matter what the ingredients are."]
 
-CHEF_LIST = ["Gordon Ramsay", "Guy Fieri", "Jamie Oliver"]
+CHEF_LIST = ["Gordon Ramsay", "Guy Fieri", "Jamie Oliver", "Burger Guy"]
 
 # App title
 st.set_page_config(page_title="Personal Chef", page_icon="👨‍🍳")
